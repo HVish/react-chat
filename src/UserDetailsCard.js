20e7fe5668fd578aa54ec5382db71efb26810748
Vishnu Singh <vishnusingh1729@gmail.com>
2017-08-20 13:23:40 +0530

    Card selection

--- src/UserDetailsCard.js
+++ src/UserDetailsCard.js
@@ -3,14 +3,36 @@ import PropTypes from 'prop-types';
 import { Card, CardActions, CardMedia, CardTitle } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import defaultImage from './user.png';
+import { cyan500, white, black } from 'material-ui/styles/colors';
 
 class UserDetailsCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { selected: false };
+    }
+
+    handleClick = (params) => {
+        this.setState({ selected: this.state.selected ? false : true });
+    };
+
+    getStyles = () => {
+        if (this.state.selected) {
+            return { card: { background: cyan500 }, title: { color: white } };
+        }
+        return { card: { background: white }, title: { color: black } };
+    }
+
     render() {
-        return <Card>
-            <CardMedia>
+        return <Card style={this.getStyles().card} onClick={this.handleClick}>
+            <CardMedia style={{ padding: 20 }}>
                 <img src={this.props.image || defaultImage} alt="" />
             </CardMedia>
-            <CardTitle title={this.props.name} subtitle={this.props.location} />
+            <CardTitle 
+                title={this.props.name} 
+                titleStyle={this.getStyles().title} 
+                subtitle={this.props.location}
+                subtitleStyle={this.getStyles().title} />
         </Card>;
     }
 };
